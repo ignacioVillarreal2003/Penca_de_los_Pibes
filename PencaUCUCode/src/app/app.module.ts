1203e4a9dd87e43c98fcdc84e7e0247382dcc2d6
ignacioVillarreal2003 <ignaciovillarreal20031231@gmail.com>
2024-04-18 16:50:03 -0300

    update

--- PencaUCUCode/src/app/app.module.ts
+++ PencaUCUCode/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { IngresoComponent } from './ingreso/ingreso.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngresoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
---
+++ PencaUCUCode/src/app/ingreso/ingreso.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingreso',
+  templateUrl: './ingreso.component.html',
+  styleUrl: './ingreso.component.css'
+})
+export class IngresoComponent {
+  username: string = "";
+  password: string = "";
+
+  RegistrarUsuario(){
+
+  }
+
+  LoginUsuario(){
+    
+  }
+}
